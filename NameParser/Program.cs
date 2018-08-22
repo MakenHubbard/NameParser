78ad38cc052e3cb101285bfeed8a99d591c36f8e
Maken Hubbard <makhub26@outlook.com>
2018-08-21 21:34:54 -0500

    adding some excercise work inclass

--- NameParser/Program.cs
+++ NameParser/Program.cs
@@ -6,7 +6,49 @@ namespace NameParser
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your first name");
+            var firstName = Console.ReadLine();
+
+            foreach (var letter in firstName)
+            {
+                Console.WriteLine(letter);
+            }
+
+            Console.WriteLine("Enter your last name");
+            var lastName = Console.ReadLine();
+            var lastNameWithSpaces = "";
+
+            foreach (var letter in lastName.ToUpper())
+            {
+                lastNameWithSpaces += letter + " ";
+            }
+
+            Console.WriteLine(lastNameWithSpaces.Trim());
+            Console.WriteLine("Do you have a middle name? (y/n)");
+
+            var hasMiddleName = Console.ReadLine();
+            var middleName = Console.ReadLine();
+
+
+            if (hasMiddleName == "y")
+            {
+                Console.WriteLine("What is your middle name");
+                var i = 0;
+
+                foreach(var letter in middleName)
+                {
+                    Console.WriteLine(letter.ToString().PadLeft(++i));
+                }
+            }
+            else
+            {
+                Console.WriteLine(@"You are dumb and shouldn't have been born. And your parents hate you.");
+            }
+
+            Console.WriteLine($"Goodbye {firstName} {middleName} {lastName}. Press Enter to exist");
+            Console.ReadLine();
+
+
         }
     }
 }
